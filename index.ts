183ba73d465c7617c087d4b9f6fb8580a8886f47
david <davidsa03@gmail.com>
2024-02-25 15:03:17 -0500

    update build to use tsup

--- index.ts
+++
@@ -1,7 +0,0 @@
-export { generateTheme } from './src/generateTheme';
-export {
-  MD3Tones,
-  MD3NeutralTones,
-  MD3Palettes,
-  MD3ColorSchemeTokens,
-} from './src/theme.types';
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { generateTheme } from './generateTheme';
+export {
+  MD3Tones,
+  MD3NeutralTones,
+  MD3Palettes,
+  MD3ColorSchemeTokens,
+} from './theme.types';
